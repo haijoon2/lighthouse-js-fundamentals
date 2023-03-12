19916915d185999a42888b066241344ce547c0ba
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-03-11 17:49:56 -0800

    Solve Lighthouse Objects Trash to Treasure Assignment

---
+++ objects-trash-to-treasure.js
@@ -0,0 +1,10 @@
+function smartGarbage(trash, bins) {
+  if (trash === "waste") {
+    bins.waste++;
+  } else if (trash === "recycling") {
+    bins.recycling++;
+  } else if (trash === "compost") {
+    bins.compost++;
+  }
+  return bins;
+}
